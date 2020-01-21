01fe6d559d0fb23ede76118bc44d13c03c30a951
eclipse_user <eclipse_user@gmail>
2020-01-21 14:00:01 +0800

    more edit..

--- src/main/java/com/atguigu/git/Happy.java
+++ src/main/java/com/atguigu/git/Happy.java
@@ -8,14 +8,17 @@
 */
 
 package com.atguigu.git;
+
 /**
- * Description:	   <br/>
- * Date:     2020年1月21日 下午1:57:00 <br/>
- * @author   xiaoyao
- * @version  
- * @see 	 
+ * Description: <br/>
+ * Date: 2020年1月21日 下午1:57:00 <br/>
+ * 
+ * @author xiaoyao
+ * @version
+ * @see
  */
 public class Happy {
-
+    public static void main(String[] args) {
+        System.out.println("Test.....");
+    }
 }
-
