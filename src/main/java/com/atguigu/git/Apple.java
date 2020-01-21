78469c63129e10874f1c3b62ed9336b7a305c124
eclipse_user <eclipse_user@gmail>
2020-01-21 13:54:51 +0800

    My first commit in eclipse

---
+++ src/main/java/com/atguigu/git/Apple.java
@@ -0,0 +1,21 @@
+/**
+ * Project Name:TestGIt
+ * File Name:Apple.java
+ * Package Name:com.atguigu.git
+ * Date:2020年1月21日下午1:16:05
+ * Copyright (c) 2020, bluemobi All Rights Reserved.
+ *
+*/
+
+package com.atguigu.git;
+/**
+ * Description:	   <br/>
+ * Date:     2020年1月21日 下午1:16:05 <br/>
+ * @author   xiaoyao
+ * @version  
+ * @see 	 
+ */
+public class Apple {
+
+}
+
---
+++ src/test/java/com/atguigu/test/GoodTest.java
@@ -0,0 +1,21 @@
+/**
+ * Project Name:TestGIt
+ * File Name:GoodTest.java
+ * Package Name:com.atguigu.test
+ * Date:2020年1月21日下午1:18:11
+ * Copyright (c) 2020, bluemobi All Rights Reserved.
+ *
+*/
+
+package com.atguigu.test;
+/**
+ * Description:	   <br/>
+ * Date:     2020年1月21日 下午1:18:11 <br/>
+ * @author   xiaoyao
+ * @version  
+ * @see 	 
+ */
+public class GoodTest {
+
+}
+
